576fd7581fdcd5d9d2390c1eb5f0251a9c2af5b7
Nk <113770164+Njuezgotswish@users.noreply.github.com>
2023-08-07 15:48:57 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,4 @@
-export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+/* eslint-disable no-unused-vars */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {});
 }
